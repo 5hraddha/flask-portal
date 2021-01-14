374607a76c8b44e63975d11b085b433a1fd033e5
Shraddha <mailmeatshraddha@gmail.com>
2021-01-13 21:15:42 -0500

    Update code with cards

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,5 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 from app import routes, models
